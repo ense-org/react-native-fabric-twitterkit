d3397ed52e1f02c5d767657226572afbc857765f
Phil Ditzler <phil.ditzler@gmail.com>
2017-02-13 16:16:36 -0500

    Twitter API request method
    - Merged `sessionDetails` and `getOAuthHeaders` from
    https://github.com/hbar-digital/react-native-fabric-twitterkit
    - Added generic `sendTwitterRequest` method for making calls to the
    Twitter REST API

--- FabricTwitterKit/FabricTwitterKit.m
+++ FabricTwitterKit/FabricTwitterKit.m
@@ -19,6 +19,11 @@
 @implementation FabricTwitterKit
 @synthesize bridge = _bridge;
 
+- (TWTRSession *)getSession {
+    TWTRSessionStore *store = [[Twitter sharedInstance] sessionStore];
+    return store.session;
+}
+
 RCT_EXPORT_MODULE();
 
 RCT_EXPORT_METHOD(login:(RCTResponseSenderBlock)callback)
@@ -37,80 +42,48 @@ RCT_EXPORT_METHOD(login:(RCTResponseSenderBlock)callback)
     }];
 }
 
+RCT_EXPORT_METHOD(getOAuthHeaders:(RCTResponseSenderBlock)callback)
+{
+    TWTRSession *session = [self getSession];
+    TWTROAuthSigning *oauthSigning = [[TWTROAuthSigning alloc] initWithAuthConfig:[Twitter sharedInstance].authConfig authSession:session];
+    NSDictionary *authHeaders = [oauthSigning OAuthEchoHeadersToVerifyCredentials];
+    callback(@[[NSNull null], authHeaders]);
+}
+
+RCT_EXPORT_METHOD(sessionDetails:(RCTResponseSenderBlock)callback)
+{
+    
+    TWTRSession *session = [self getSession];
+    if (session) {
+        NSDictionary *details = @{
+                                  @"authToken": session.authToken,
+                                  @"authTokenSecret": session.authTokenSecret,
+                                  @"userID":session.userID,
+                                  @"userName":session.userName
+                                  };
+        callback(@[[NSNull null], details]);
+    } else {
+        callback(@[[NSNull null], [NSNull null]]);
+    }
+}
+
 RCT_EXPORT_METHOD(fetchProfile:(RCTResponseSenderBlock)callback)
 {
     TWTRAPIClient *client = [[TWTRAPIClient alloc] init];
-    TWTRSessionStore *store = [[Twitter sharedInstance] sessionStore];
-
-    TWTRSession *lastSession = store.session;
-
+    
+    TWTRSession *lastSession = [self getSession];
+    
     if(lastSession) {
         NSString *showEndpoint = @"https://api.twitter.com/1.1/users/show.json";
         NSDictionary *params = @{@"user_id": lastSession.userID};
-
+        
         NSError *clientError;
         NSURLRequest *request = [client
                                  URLRequestWithMethod:@"GET"
                                  URL:showEndpoint
                                  parameters:params
                                  error:&clientError];
-
-          if (request) {
-            [client
-             sendTwitterRequest:request
-             completion:^(NSURLResponse *response,
-                          NSData *data,
-                          NSError *connectionError) {
-                 if (data) {
-                     // handle the response data e.g.
-                     NSError *jsonError;
-                     NSDictionary *json = [NSJSONSerialization
-                                           JSONObjectWithData:data
-                                           options:0
-                                           error:&jsonError];
-                     NSLog(@"%@",[json description]);
-                     callback(@[[NSNull null], json]);
-                 }
-                 else {
-                     NSLog(@"Error code: %ld | Error description: %@", (long)[connectionError code], [connectionError localizedDescription]);
-                     callback(@[[connectionError localizedDescription]]);
-                 }
-             }];
-        }
-        else {
-            NSLog(@"Error: %@", clientError);
-        }
-
-    }
-
-
-}
-
-RCT_EXPORT_METHOD(fetchTweet:(NSDictionary *)options :(RCTResponseSenderBlock)callback)
-{
-    TWTRAPIClient *client = [[TWTRAPIClient alloc] init];
-    TWTRSessionStore *store = [[Twitter sharedInstance] sessionStore];
-    NSString *id = options[@"id"];
-    NSString *trim_user = options[@"trim_user"];
-    NSString *include_my_retweet = options[@"include_my_retweet"];
-
-    TWTRSession *lastSession = store.session;
-
-    if(lastSession) {
-        NSString *showEndpoint = @"https://api.twitter.com/1.1/statuses/show.json";
-        NSDictionary *params = @{
-                                    @"id": id,
-                                    @"trim_user": trim_user,
-                                    @"include_my_retweet": include_my_retweet
-                                };
-
-        NSError *clientError;
-        NSURLRequest *request = [client
-                                 URLRequestWithMethod:@"GET"
-                                 URL:showEndpoint
-                                 parameters:params
-                                 error:&clientError];
-
+        
         if (request) {
             [client
              sendTwitterRequest:request
@@ -136,35 +109,121 @@ RCT_EXPORT_METHOD(fetchTweet:(NSDictionary *)options :(RCTResponseSenderBlock)ca
         else {
             NSLog(@"Error: %@", clientError);
         }
-
+        
     }
+    
+    
+}
 
+RCT_EXPORT_METHOD(sendTwitterRequest:(NSDictionary *)options :(RCTResponseSenderBlock)callback)
+{
+    TWTRSession *lastSession = [self getSession];
+    NSString *userID = lastSession.userID;
+    TWTRAPIClient *client = [[TWTRAPIClient alloc] initWithUserID:userID];
+    NSString *endpoint = options[@"endpoint"];
+    NSString *method = options[@"method"];
+    NSDictionary *params = options[@"params"];
+    
+    NSError *clientError;
+    NSURLRequest *request = [client URLRequestWithMethod:method URL:endpoint parameters:params error:&clientError];
+    
+    if (request) {
+        [client sendTwitterRequest:request completion:^(NSURLResponse *response, NSData *data, NSError *connectionError) {
+            if (data) {
+                NSError *jsonError;
+                NSDictionary *json = [NSJSONSerialization JSONObjectWithData:data options:0 error:&jsonError];
+                callback(@[[NSNull null], json]);
+            }
+            else {
+                NSLog(@"Error: %@", connectionError);
+                callback(@[[connectionError localizedDescription]]);
+            }
+        }];
+    }
+    else {
+        NSLog(@"Error: %@", clientError);
+        callback(@[[clientError localizedDescription]]);
+    }
+}
+
+RCT_EXPORT_METHOD(fetchTweet:(NSDictionary *)options :(RCTResponseSenderBlock)callback)
+{
+    TWTRAPIClient *client = [[TWTRAPIClient alloc] init];
+    NSString *id = options[@"id"];
+    NSString *trim_user = options[@"trim_user"];
+    NSString *include_my_retweet = options[@"include_my_retweet"];
+    
+    TWTRSession *lastSession = [self getSession];
+    
+    if(lastSession) {
+        NSString *showEndpoint = @"https://api.twitter.com/1.1/statuses/show.json";
+        NSDictionary *params = @{
+                                 @"id": id,
+                                 @"trim_user": trim_user,
+                                 @"include_my_retweet": include_my_retweet
+                                 };
+        
+        NSError *clientError;
+        NSURLRequest *request = [client
+                                 URLRequestWithMethod:@"GET"
+                                 URL:showEndpoint
+                                 parameters:params
+                                 error:&clientError];
+        
+        if (request) {
+            [client
+             sendTwitterRequest:request
+             completion:^(NSURLResponse *response,
+                          NSData *data,
+                          NSError *connectionError) {
+                 if (data) {
+                     // handle the response data e.g.
+                     NSError *jsonError;
+                     NSDictionary *json = [NSJSONSerialization
+                                           JSONObjectWithData:data
+                                           options:0
+                                           error:&jsonError];
+                     NSLog(@"%@",[json description]);
+                     callback(@[[NSNull null], json]);
+                 }
+                 else {
+                     NSLog(@"Error code: %ld | Error description: %@", (long)[connectionError code], [connectionError localizedDescription]);
+                     callback(@[[connectionError localizedDescription]]);
+                 }
+             }];
+        }
+        else {
+            NSLog(@"Error: %@", clientError);
+        }
+        
+    }
+    
 }
 
 RCT_EXPORT_METHOD(composeTweet:(NSDictionary *)options :(RCTResponseSenderBlock)callback) {
-
+    
     NSString *body = options[@"body"];
-
+    
     TWTRComposer *composer = [[TWTRComposer alloc] init];
-
+    
     if (body) {
         [composer setText:body];
     }
-
+    
     UIViewController *rootView = [UIApplication sharedApplication].keyWindow.rootViewController;
     [composer showFromViewController:rootView completion:^(TWTRComposerResult result) {
-
+        
         bool completed = NO, cancelled = NO, error = NO;
-
+        
         if (result == TWTRComposerResultCancelled) {
             cancelled = YES;
         }
         else {
             completed = YES;
         }
-
+        
         callback(@[@(completed), @(cancelled), @(error)]);
-
+        
     }];
 }
 
@@ -172,7 +231,7 @@ RCT_EXPORT_METHOD(logOut)
 {
     TWTRSessionStore *store = [[Twitter sharedInstance] sessionStore];
     NSString *userID = store.session.userID;
-
+    
     [store logOutUserID:userID];
 }
 
