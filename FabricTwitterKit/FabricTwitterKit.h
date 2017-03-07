2ce1c17fa3a010ca74fb383d2ac4df797bb67713
Phil Ditzler <wannatouchmyfro2@gmail.com>
2017-03-07 02:03:42 -0500

    Updated imports for react-native 0.40+

--- FabricTwitterKit/FabricTwitterKit.h
+++ FabricTwitterKit/FabricTwitterKit.h
@@ -5,8 +5,7 @@
 //  Created by Trevor Porter on 8/1/16.
 //  Copyright © 2016 Trevor Porter. All rights reserved.
 //
-
-#import "RCTBridgeModule.h"
+#import <React/RCTBridgeModule.h>
 
 @interface FabricTwitterKit : NSObject <RCTBridgeModule> {
     RCTResponseSenderBlock _callback;
